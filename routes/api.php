98dd0a8068d8d95b62f1e3b1a04bd27944efda68
iPeluwa <ipeluwa@gmail.com>
2021-04-14 18:42:26 +0100

    ALL

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Middleware\RequireWorkspace;
+use Illuminate\Support\Facades\Route;
+use Sendportal\Base\Facades\Sendportal;
+
+Route::middleware([
+    config('sendportal-host.throttle_middleware'),
+    RequireWorkspace::class
+])->group(function() {
+
+    // Auth'd API routes (workspace-level auth!).
+    Sendportal::apiRoutes();
+
+});
+
+// Non-auth'd API routes.
+Sendportal::publicApiRoutes();
