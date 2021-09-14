58ebafea5b7e88943f7eb1bda652d0db246a6719
iPeluwa <ipeluwa@gmail.com>
2021-09-14 12:04:05 +0100

    .

--- vendor/mettle/sendportal-core/src/Http/Controllers/Api/SubscribersController.php
+++ vendor/mettle/sendportal-core/src/Http/Controllers/Api/SubscribersController.php
@@ -52,7 +52,7 @@ class SubscribersController extends Controller
         $subscriber = $this->apiService->storeOrUpdate($workspaceId, collect($request->validated()));
 
         $subscriber->load('tags');
-        return response()->json([
+        return (new SubscriberResource($subscriber))->response()->json([
             'message' => 'Ta-da!!, Your account has been created successfully 🥰. Verification email has been sent!',
         ]);
               
